4b2d5a132b60dd1421c7a1af28f4a55a6c4aeba8
Michael Leonffu <leonffu.michael@gmail.com>
2016-06-10 12:17:51 -0700

    Added comments and cleaned up code
    Also worked on better start command

--- LarypieBot.js
+++ LarypieBot.js
@@ -1,16 +1,18 @@
-// Program Details for
-var programDetails = [
-"Larypie Bot",
-"Version 5.6.1",
-"Discord Js"
-];
-console.log("--------------------");
-for (var i = 0; i < programDetails.length; i++) {
-  console.log(programDetails[i]);
-};
-console.log("--------------------");
-//vars
-  //Msg
+// Program Details for program
+  var programDetails = [
+  "Larypie Bot",
+  "Version 6.0.0",
+  "Discord Js"
+  ];
+//Display the Prgram details
+  console.log("--------------------");
+  //Cycles though the indexes of the array for Program Detail
+  for (var i = 0; i < programDetails.length; i++) {
+    console.log(programDetails[i]);
+  };
+  console.log("--------------------");
+//Varibles
+  //Messages
     //Terminate
       var commandTerminate = 0,
       checkTerminate = 0;
@@ -19,61 +21,69 @@ console.log("--------------------");
       var Discord = require("discord.js");
       // Create a client
       var larypieBot = new Discord.Client();
-    //Songs
+    //Song playlist
       var song = [
-       "Lux",
-       "+play https://www.youtube.com/watch?v=VTaOzFVZIIY",
+        "Name of Song",
+        "+play and the song url",
+        "Lux",
+        "+play https://www.youtube.com/watch?v=VTaOzFVZIIY",
         "Temmo",
         "+play https://www.youtube.com/watch?v=KDIAGsCOWD8",
-       "Braum",
-       "+play https://www.youtube.com/watch?v=zYd8D804bT0",
-       "Windmill",
-       "+play https://www.youtube.com/watch?v=41kF2-JvwPs",
-       "Undertale",
-       "+play https://www.youtube.com/watch?v=ZcoqR9Bwx1Y"
+        "Braum",
+        "+play https://www.youtube.com/watch?v=zYd8D804bT0",
+        "Windmill",
+        "+play https://www.youtube.com/watch?v=41kF2-JvwPs",
+        "Undertale",
+        "+play https://www.youtube.com/watch?v=ZcoqR9Bwx1Y"
       ],
+      //Other varibles for songs
       songSelect = 1,
-      songToggLoop = false,
-      youToggLoop = false,
+      songToggLoop = false;
+    //Youtube All Play
+      var youToggLoop = false,
       youPlay = "https://www.youtube.com/watch?v=ADmSA0eE8SU";
-    //Messaging Array
+    //Messaging Arrays
+      //Natrual Message array
       var messArrayNat = [
-        "SAMPLE:Message",
-         "RESPONCE:Message",
-        //Messageing Basic
-          "ping",
-          "Pong",
+        "INPUT:Message",
+        "RESPONCE:Message",
 
-          "you",
-          "No u.",
+        "ping",
+        "Pong",
 
-          " u ",
-          "No u.",
+        "you",
+        "No u.",
 
-          "him",
-          "'him'... Why not her...",
+        " u",
+        "No u.",
 
-          "will",
-          "And I will make America great",
+        "him",
+        "'him'... Why not her...",
 
-          "who",
-          "ME!!!!!!!!!!!!!!!!!!!!",
+        "will",
+        "And I will make America great",
 
-          "hello",
-          "HIIIIIIIII!",
+        "who",
+        "ME!!!!!!!!!!!!!!!!!!!!",
 
-          " help ",
-          "Not anyone can help you. [consider >help]",
-        ],
-        messArrayComm = [
-          "help",
-          "LOL STILL NOT DEVELOPED!",
+        "hello",
+        "HIIIIIIIII!",
 
-          "channelid",
-          "HA NOT DEVELOPED YET!"
-        ];
+        " help ",
+        "Not anyone can help you. [consider >help]",
+      ],
+      //Command Message array
+      messArrayComm = [
+        "MESSAGE;DO NOT USE >; Message",
+        "RESPONCE: Message",
+
+        "help",
+        "LOL STILL NOT DEVELOPED!",
+
+        "channelid",
+        "HA NOT DEVELOPED YET!"
+      ];
       //Messaging Array Referance
-
 // This code will run once the bot has started up.
 larypieBot.on("ready", function () {
   console.log("Ready to begin! Serving in " + larypieBot.channels.length + " channels");
@@ -87,12 +97,23 @@ larypieBot.on("disconnected", function () {
 });
 // This code will run once the bot receives any message.
 larypieBot.on("message", function (msg) {
-  larypieBot.joinVoiceChannel(msg.author.voiceChannel);
   //Messageing Array
     //Natrual
+      //Loops Array Messaging System starts after Example messages in Array
       for (var i = 2; messArrayNat.length > i && i > 0; i+=2) {
+        //Checks if any message inputs have been matched (the first of every pair)
         if(msg.content.toLowerCase().includes(messArrayNat[i]) && msg.author != larypieBot.user){
+          //If message Input is matched then respond with responce (secound of every pair)
           larypieBot.sendMessage(msg.channel,messArrayNat[i+1]);
+          //Stop checking for messages at this point bot only says one thing to every message.
+          i = -1;
+        }
+      };
+    //Commands
+      //Same as above "for" for now 
+      for (var i = 2; messArrayComm.length > i && i > 0; i+=2) {
+        if(msg.content.toLowerCase().indexOf(">" + messArrayComm[i]) === 0 && msg.author != larypieBot.user){
+          larypieBot.sendMessage(msg.channel,messArrayComm[i+1]);
           i = -1;
         }
       };
@@ -170,19 +191,8 @@ larypieBot.on("message", function (msg) {
     if(msg.content.indexOf(">Play Last") != -1){
       this.sendMessage(msg.channel, "+play " + youPlay);
     }
-    if(msg.content.indexOf(">del") === 0 ){
-      larypieBot.deleteMessage(msg.id);
-      larypieBot.deleteMessage(msg.content);
-      larypieBot.deleteMessage(msg.server);
-      larypieBot.deleteMessage(msg.channel);
-      larypieBot.sendMessage(msg.channel,"completed!");
-      larypieBot.getChannelLogs(msg.channel);
-      larypieBot.sendMessage(msg.channel,larypieBot.getChannelLogs(msg.channel));
-
-    }
-
 });
-
-// Login (replace these auth details with your bot's)
+// Login
 larypieBot.loginWithToken("MTg5OTI3NDMxNDMwMzQwNjA4.CjkUWg.9kAzFEGq-ZtGCDkYvyoY5Fhle2s");
+//Alert Console that JS file ran completely
 console.log("Program: " + programDetails[0] + programDetails[1] + " JS file complete.");
